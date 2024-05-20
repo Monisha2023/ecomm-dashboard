bff1239300202e5e820c1f7af20a739e512f0b7f
Monisha <monishachak1234@gmail.com>
2024-05-20 16:47:05 +0530

    created layout of ecom dashboard

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
-import logo from './logo.svg';
+
 import './App.css';
-import { Button } from 'react-bootstrap';
+
+import Header from './Header';
+import { BrowserRouter,Route,Routes } from 'react-router-dom';
+import Login from './Login';
+import Register from './Register';
+import AddProduct from './AddProduct';
+import UpdateProduct from './UpdateProduct';
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
+      <Header />
       <h1>E-Comm Project</h1>
-      <button>Normal Button</button>
-      <Button>Bootstrap Button</Button>
+      <Routes>
+      <Route path="/login" element={<Login />}>Login</Route>
+      <Route path="/register" element={<Register />}>Register</Route>
+      <Route path="/add" element={<AddProduct />}>Add</Route>
+      <Route path="/update" element={<UpdateProduct />}>Update</Route>
+      </Routes>
+      </BrowserRouter>
+      
+      
     </div>
   );
 }
